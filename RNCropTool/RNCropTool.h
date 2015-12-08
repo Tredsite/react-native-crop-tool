4475cf8f2f39370473e06f748c14846f78f6a4a5
Andrew Crowell <andrewj.crowell@gmail.com>
2015-12-07 18:55:27 -0800

    Initial Commit

---
+++ RNCropTool/RNCropTool.h
@@ -0,0 +1,13 @@
+//
+//  RNCropTool.h
+//  RNCropTool
+//
+//  Created by Andrew Crowell on 12/7/15.
+//  Copyright © 2015 TRED. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNCropTool : NSObject
+
+@end
---
+++ RNCropTool/RNCropTool.m
@@ -0,0 +1,13 @@
+//
+//  RNCropTool.m
+//  RNCropTool
+//
+//  Created by Andrew Crowell on 12/7/15.
+//  Copyright © 2015 TRED. All rights reserved.
+//
+
+#import "RNCropTool.h"
+
+@implementation RNCropTool
+
+@end
