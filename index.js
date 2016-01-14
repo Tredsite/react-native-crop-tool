a8388f4835bb896eab65e131ea0ed6db260dd388
Andrew Crowell <andrewj.crowell@gmail.com>
2016-01-14 09:26:14 -0800

    Use a callback for android.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-var RNCropTool = require('react-native').NativeModules.RNCropTool;
+var React = require('react-native');
+var {
+  Platform,
+  NativeModules,
+} = React;
+var RNCropTool = NativeModules.RNCropTool;
 
 module.exports = {
   crop: function(imagePath, hFactor, wFactor) {
-    return RNCropTool.crop(imagePath, hFactor, wFactor);
+    if (Platform.OS === 'android') {
+      return new Promise(function(resolve, reject) {
+        RNCropTool.crop(imagePath, hFactor, wFactor, function(err, data) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data);
+          }
+        });
+      });
+    } else {
+      return RNCropTool.crop(imagePath, hFactor, wFactor);
+    }
   }
 };
+
